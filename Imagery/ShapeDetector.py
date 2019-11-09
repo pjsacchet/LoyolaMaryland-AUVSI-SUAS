25afb556db40887a7964e61fc3757726ed58ebd8
Caleb Renfroe <caleb1214@aol.com>
2019-11-09 13:07:45 -0500

    Commented.

--- Imagery/ShapeDetector.py
+++ Imagery/ShapeDetector.py
@@ -5,11 +5,22 @@ class ShapeDetector:
 	def __init__(self):
 		pass
 
+	#According to opencv API
+	#https://docs.opencv.org/2.4/modules/imgproc/doc/structural_analysis_and_shape_descriptors.html?highlight=approxpolydp#cv2.approxPolyDP
+	#approxPolyDP(c,epsilon,Closed)
+	# c (curve) - input vector of a 2D point stored in a numpy array
+	#0.04 * peri -(epsilon – Parameter specifying the approximation accuracy. This is the maximum distance between the original curve and its approximation.
+	#Truse (closed -If true, the approximated curve is closed (its first and last vertices are connected).)
+	#The functions approxPolyDP approximate a curve or a polygon with another curve/polygon with less vertices so that the distance between them is less or equal to the specified precision. It uses the Douglas-Peucker algorithm http://en.wikipedia.org/wiki/Ramer-Douglas-Peucker_algorithm
+	#Detects what the shape is
 	def detect(self, c):
 		# initialize the shape name and approximate the contour
 		shape = "unidentified"
 		peri = cv2.arcLength(c, True)
 		approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+
+		##Print to see the output of this
+		printf("Approx = " approx)
 
 		# if the shape is a triangle, it will have 3 vertices
 		if len(approx) == 3:
@@ -20,7 +31,7 @@ class ShapeDetector:
 		elif len(approx) == 4:
 			# compute the bounding box of the contour and use the
 			# bounding box to compute the aspect ratio
-			(x, y, w, h) = cv2.boundingRect(approx)
+			(x, y, w, h) = cv2.boundingRect(approx) #CV2 - The function calculates and returns the minimal up-right bounding rectangle for the specified point set.
 			ar = w / float(h)
 
 			# a square will have an aspect ratio that is approximately
