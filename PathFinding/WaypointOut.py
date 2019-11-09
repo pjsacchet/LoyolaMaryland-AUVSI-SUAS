27b6254b3234d1b0777d6d91d148eec609ad0ab3
Patrick Sacchet <pjsacchet@loyola.edu>
2019-11-08 20:49:04 -0500

    Waypoint output generator added

---
+++ PathFinding/WaypointOut.py
@@ -0,0 +1,23 @@
+# File is set to parse judge given waypoint file and output correctly formatted waypoints for our drone
+# Authors:
+    # Patrick Sacchet
+# Date: 11/8/19
+
+
+
+
+
+
+
+
+
+
+
+
+# Main functionality 
+def main():
+    print("Test")
+    return 0;
+
+if __name__ == "__main__":
+    main()
