be59bc3ff8c77a99eebd90b1a0849dd57eba8ab4
ptbuchanan <ptbuchanan@loyola.edu>
2019-11-15 15:14:31 -0500

    Fixing

--- Imagery/ProcessShapes.py
+++ Imagery/ProcessShapes.py
@@ -9,72 +9,88 @@ import imutils
 import cv2
 import pdb
 
-## Input: image file
- # Output: None
- # Purpose: Reads the image to see if it exists
-##
-# ArgumentParser() turns the argument value from a string to an object
-ap = argparse.ArgumentParser()
-# Taking a string from user and turning it into an object
-ap.add_argument("-i", "--image", required=True, help="path to the input image")
-args = vars(ap.parse_args())
+#def main():
+  ## Input: image file
+   # Output: None
+   # Purpose: Reads the image to see if it exists
+  ##
+  # ArgumentParser() turns the argument value from a string to an object
+  def takingImage():
+    ap = argparse.ArgumentParser()
+    # Taking a string from user and turning it into an object
+    ap.add_argument("-i", "--image", required=True, help="path to the input image")
+    args = vars(ap.parse_args())
 
-## Input: None
- # Output: None
- # Purpose: Load the image and resize it to a smaller factor so that
- # the shapes can be approximated better
-##
-# Returns a NumPy array representing the image
-image = cv2.imread(args["image"])
-# Image being rezied
-resized = imutils.resize(image, width=300)
-ratio = image.shape[0] / float(resized.shape[0])
+  ## Input: None
+   # Output: None
+   # Purpose: Load the image and resize it to a smaller factor so that
+   # the shapes can be approximated better
+  ##
+  # Returns a NumPy array representing the image
+  def processImage():
+    image = cv2.imread(args["image"])
+    # Image being rezied
+    resized = imutils.resize(image, width=300)
+    ratio = image.shape[0] / float(resized.shape[0])
 
-## Input: None
- # Output: None
- # Purpose: Converts the resized image to grayscale, blur it slightly,
- # and threshold it
-##
-gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+  ## Input: None
+   # Output: None
+   # Purpose: Converts the resized image to grayscale, blur it slightly,
+   # and threshold it
+  ##
+  def refaction(resized):
+    gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
 
-## Input:None
- # Output: None
- # Purpose: Find contours in the thresholded image and initialize the
- # shape detector
-##
-# thresh.copy() is source image, cv2.RETR_EXTERNAL is contour retrieval mode,
-# cv2.CHAIN_APPROX_SIMPLE is contour approximation method
-# Essentially stores the (x,y) coordinates of the boundary of a shape
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-  cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-sd = ShapeDetector()
+  ## Input:None
+   # Output: None
+   # Purpose: Find contours in the thresholded image and initialize the
+   # shape detector
+  ##
+  # thresh.copy() is source image, cv2.RETR_EXTERNAL is contour retrieval mode,
+  # cv2.CHAIN_APPROX_SIMPLE is contour approximation method
+  # Essentially stores the (x,y) coordinates of the boundary of a shape
+  def findCordinates(cnts):
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+    cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    sd = ShapeDetector()
 
-## Input: Name of an image
- # Output: None
- # Purpose: loop over the contours
-##
-for c in cnts:
-  # Compute the center of the contour, then detect the name of the
-  # shape using only the contour
-  M = cv2.moments(c)
-  cX = int((M["m10"] / M["m00"]) * ratio)
-  cY = int((M["m01"] / M["m00"]) * ratio)
-  shape = sd.detect(c)
+  ## Input: Name of an image
+   # Output: None
+   # Purpose: loop over the contours
+  ##
+  def computeContour(cnts):
+    for c in cnts:
+      # Compute the center of the contour, then detect the name of the
+      # shape using only the contour
+      M = cv2.moments(c)
+      cX = int((M["m10"] / M["m00"]) * ratio)
+      cY = int((M["m01"] / M["m00"]) * ratio)
+      shape = sd.detect(c)
 
-  # Multiply the contour (x, y)-coordinates by the resize ratio,
-  # then draw the contours and the name of the shape on the image
-  c = c.astype("float")
-  c *= ratio
-  c = c.astype("int")
-  cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-  cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-     0.5, (255, 255, 255), 2)
+    # Multiply the contour (x, y)-coordinates by the resize ratio,
+    # then draw the contours and the name of the shape on the image
 
-  # Displays the output image
-  cv2.imshow("Image", image)
-  cv2.waitKey(0)
+      c = c.astype("float")
+      c *= ratio
+      c = c.astype("int")
+      cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+      cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+        0.5, (255, 255, 255), 2)
+
+  def displayImage(image):
+    # Displays the output image
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+  return image
+
+
+if __name__ == "__main__":
+  main()
+  takingImage()
 
 # Do we care about what the letter is
+# Break up code into functions
+# Find out why image 3 is not loved
