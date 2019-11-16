2b80bbb9a494850a10d5b670f61c23e87e1c2168
Patrick Sacchet <pjsacchet@loyola.edu>
2019-11-15 21:44:43 -0500

    Basic setting functionality for coordinates implemented

--- PathFinding/Waypoint.py
+++ PathFinding/Waypoint.py
@@ -1,4 +1,3 @@
-# Class represents a waypoint given in the competition. Funcitonality will allow user to manipulate input given via command line
 # Date - 11/15/19
 # Contributers:
     # Patrick Sacchet (pjsacchet)
@@ -6,12 +5,44 @@
 import sys
 import os
 
+# Class represents a waypoint given in the competition. Funcitonality will allow user to manipulate input given via command line
 class Waypoint:
 
-    def __init__(self):
-        self.waypoint = 0
+    # Constructor for a waypoint object
+    # Input:
+        # x_cord - x coorindate of the waypoint
+        # y_cord - y cooridnate of the waypoint
+        # z_cord - z coorindate of the waypoint
+    def __init__(self, x_cord, y_cord, z_cord):
+        self.x_cord = x_cord
+        self.y_cord = y_cord
+        self.z_cord = z_cord
         return
 
+    # Simple print statement conversions
+    # Input: None
+    # Returns: String with x y and z coordinates
     def __str__(self):
         str_coord = "This is my x coordinate: " + str(self.waypoint)
         return str_coord
+
+    # Sets new x coordinate
+    # Input: x_cord - new x coordinate
+    # Returns: None
+    def set_x_cord(x_cord):
+        self.x_cord = x_cord
+        return
+
+    # Sets new y coordinate
+    # Input: y_cord - new y coordinate
+    # Returns: None
+    def set_y_cord(y_cord):
+        self.y_cord = y_cord
+        return
+
+    # Sets new z coordinate
+    # Input: z_cord - new z coordinate
+    # Returns: None 
+    def set_z_cord(z_cord):
+        self.z_cord = z_cord
+        return
