8c055fe9dee00b31de74e30dce4914143a196b07
ptbuchanan <ptbuchanan@loyola.edu>
2019-11-16 11:55:11 -0500

    Debug

--- Imagery/ProcessShapes.py
+++ Imagery/ProcessShapes.py
@@ -1,5 +1,5 @@
 # Import the necessary packages
-#from pyimagesearch.shapedetector import ShapeDetector
+# from pyimagesearch.shapedetector import ShapeDetector
 from ShapeDetector import ShapeDetector
 # Allows the user to use command line interface
 import argparse
@@ -15,7 +15,7 @@ import pdb
    # Purpose: Reads the image to see if it exists
   ##
   # ArgumentParser() turns the argument value from a string to an object
-  def takingImage():
+def takingImage():
     ap = argparse.ArgumentParser()
     # Taking a string from user and turning it into an object
     ap.add_argument("-i", "--image", required=True, help="path to the input image")
@@ -27,41 +27,41 @@ import pdb
    # the shapes can be approximated better
   ##
   # Returns a NumPy array representing the image
-  def processImage():
+def processImage(args, ratio):
     image = cv2.imread(args["image"])
     # Image being rezied
     resized = imutils.resize(image, width=300)
     ratio = image.shape[0] / float(resized.shape[0])
 
-  ## Input: None
-   # Output: None
-   # Purpose: Converts the resized image to grayscale, blur it slightly,
-   # and threshold it
-  ##
-  def refaction(resized):
+## Input: None
+ # Output: None
+ # Purpose: Converts the resized image to grayscale, blur it slightly,
+ # and threshold it
+##
+def refaction(resized):
     gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
     thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
 
-  ## Input:None
-   # Output: None
-   # Purpose: Find contours in the thresholded image and initialize the
-   # shape detector
-  ##
-  # thresh.copy() is source image, cv2.RETR_EXTERNAL is contour retrieval mode,
-  # cv2.CHAIN_APPROX_SIMPLE is contour approximation method
-  # Essentially stores the (x,y) coordinates of the boundary of a shape
-  def findCordinates(cnts):
+## Input:None
+ # Output: None
+ # Purpose: Find contours in the thresholded image and initialize the
+ # shape detector
+##
+ # thresh.copy() is source image, cv2.RETR_EXTERNAL is contour retrieval mode,
+ # cv2.CHAIN_APPROX_SIMPLE is contour approximation method
+ # Essentially stores the (x,y) coordinates of the boundary of a shape
+def findCordinates(cnts, thresh):
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
     cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     sd = ShapeDetector()
 
-  ## Input: Name of an image
-   # Output: None
-   # Purpose: loop over the contours
-  ##
-  def computeContour(cnts):
+## Input: Name of an image
+ # Output: None
+ # Purpose: loop over the contours
+##
+def computeContour(cnts, sd, ratio, image):
     for c in cnts:
       # Compute the center of the contour, then detect the name of the
       # shape using only the contour
@@ -69,27 +69,29 @@ import pdb
       cX = int((M["m10"] / M["m00"]) * ratio)
       cY = int((M["m01"] / M["m00"]) * ratio)
       shape = sd.detect(c)
-
     # Multiply the contour (x, y)-coordinates by the resize ratio,
     # then draw the contours and the name of the shape on the image
-
       c = c.astype("float")
       c *= ratio
       c = c.astype("int")
       cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-      cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-        0.5, (255, 255, 255), 2)
+      cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-  def displayImage(image):
+def displayImage(image):
     # Displays the output image
     cv2.imshow("Image", image)
     cv2.waitKey(0)
-  return image
+    return
 
 
 if __name__ == "__main__":
-  main()
+ # main()
   takingImage()
+  processImage("image")
+  refaction()
+  findCordinates()
+  computeContour()
+  displayImage()
 
 # Do we care about what the letter is
 # Break up code into functions
